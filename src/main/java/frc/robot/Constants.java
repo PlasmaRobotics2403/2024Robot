4dbfa2a46662dab4315da52d407f5cc432c046a2
ethan <ethan@diavida.com>
2024-01-29 19:22:59 -0700

    created intake class

---
+++ src/main/java/frc/robot/Constants.java
@@ -0,0 +1,14 @@
+package frc.robot;
+
+public class Constants {
+    
+    public class IntakeConstants {
+    
+        public static final int rollerID = 0;
+        public static final double rollerSpeed = 0.5;
+    }
+
+    public class RobotConstants {
+        public static final int driverJoystickID = 0;
+    }
+}
