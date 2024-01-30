3b9aaf08a6f3693e969d9da796b51f211cafab79
ethan <ethan@diavida.com>
2024-01-29 18:34:54 -0700

    blank timed robot base

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -4,21 +4,9 @@
 
 package frc.robot;
 
-import edu.wpi.first.cameraserver.CameraServer;
-import edu.wpi.first.wpilibj.Compressor;
-import edu.wpi.first.wpilibj.DriverStation;
-import edu.wpi.first.wpilibj.PneumaticsModuleType;
 import edu.wpi.first.wpilibj.TimedRobot;
-import edu.wpi.first.wpilibj.DriverStation.Alliance;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
-import frc.robot.auto.modes.*;
-import frc.robot.auto.util.AutoMode;
-import frc.robot.auto.util.AutoModeRunner;
-import frc.robot.controllers.PlasmaGuitar;
-import frc.robot.controllers.PlasmaJoystick;
-
-
 
 /**
  * The VM is configured to automatically run this class, and to call the functions corresponding to
@@ -27,130 +15,20 @@ import frc.robot.controllers.PlasmaJoystick;
  * project.
  */
 public class Robot extends TimedRobot {
-  PlasmaJoystick driver;
-  PlasmaGuitar navigator;
+  private static final String kDefaultAuto = "Default";
+  private static final String kCustomAuto = "My Auto";
+  private String m_autoSelected;
+  private final SendableChooser<String> m_chooser = new SendableChooser<>();
 
-  Swerve swerve;
-  Limelight limelight;
-  Elevator elevator;
-  Grabber grabber;
-  Intake intake;
-
-  Compressor compressor;
-
-  double elevatorTarget;
-  double armTarget;
-  double extenderTarget;
-  double grabberTarget;
-  double intakeTarget;
-
-  String robotState;
-  String currentState;
-
-  AutoModeRunner autoModeRunner;
-  AutoMode[] autoModes;
-  int autoModeSelection;
-  SendableChooser<Integer> autoChooser;
-  
-  LEDs leds;
-  int hue;
-  int value;
-  boolean FMS_Connected;
-  boolean passthrough = false;
-
-  boolean attemptingConeGrab = false;
-  boolean intakeCone = false;
-
-  String gamePiece;
   /**
    * This function is run when the robot is first started up and should be used for any
    * initialization code.
    */
   @Override
   public void robotInit() {
-    driver = new PlasmaJoystick(Constants.DRIVER_JOYSTICK_PORT);
-    navigator = new PlasmaGuitar(Constants.COPILOT_JOYSTICK_PORT);
-
-    compressor = new Compressor(31, PneumaticsModuleType.REVPH);  
-
-    swerve = new Swerve();
-    limelight = new Limelight();
-    elevator = new Elevator();
-    intake = new Intake();
-    grabber = new Grabber();
-
-    elevatorTarget = 0;
-    armTarget = 5;
-
-   
-    autoModeRunner = new AutoModeRunner();
-    autoModes = new AutoMode[20];
-    autoModes[0] = new Nothing();
-    autoModes[1] = new ScoringTableToCharge(swerve, elevator, grabber);
-    autoModes[2] = new AudienceToCharge(swerve, elevator, grabber);
-    autoModes[3] = new LeaveCommunity(swerve, elevator, grabber);
-    autoModes[4] = new Score(swerve, elevator, grabber);
-    autoModes[5] = new TwoCubeAuto(intake, grabber, elevator, swerve);
-    autoModes[6] = new TwoCubeAutoOverCable(intake, grabber, elevator, swerve);
-    autoModes[7] = new OverChargedStation(swerve, elevator, grabber);
-    autoModes[8] = new OverChargedStationCone(swerve, elevator, grabber);
-    autoModes[9] = new OverChargedStationCubeGrab(swerve, elevator, grabber, intake);
-    autoModes[10] = new OverChargedStationConeGrab(swerve, elevator, grabber, intake);
-    // april tag locations
-    // tag #1 = red cable
-    // tag #2 = red charged station
-    // tag #3 = red clear
-    // tag #4 = blue feeder
-    // tag #5 = red feeder
-    // tag #6 = blue clear
-    // tag #7 = blue charged station
-    // tag #8 = blue cable
-
-    autoChooser = new SendableChooser<>();
-    autoChooser.setDefaultOption("nothing", 0);
-    SmartDashboard.putNumber("Nothing", 0);
-    SmartDashboard.putNumber("Scoringtable Side Charge", 1);
-    SmartDashboard.putNumber("Audience Side Charge", 2);
-    SmartDashboard.putNumber("Leave Community", 3);
-    SmartDashboard.putNumber("Score", 4);
-    SmartDashboard.putNumber("Two Cube Auto", 5);
-    SmartDashboard.putNumber("Two Cube Auto Bump Side", 6);
-    SmartDashboard.putNumber("Over Charge Station Place Cube", 7);
-    SmartDashboard.putNumber("Over Charge Station Place Cone", 8);
-    SmartDashboard.putNumber("Over Charge Station Place Cube and Grab Cube", 9);
-    SmartDashboard.putNumber("Over Charge Station Place Cone and Grab Cone", 10);
-
-    limelight.logging();
-
-    if(limelight.getApriltag() == 2 || limelight.getApriltag() == 7) {
-      autoChooser.addOption("Balance", 4);
-      autoChooser.addOption("One Cube Auto", 5);
-    }
-    else if(limelight.getApriltag() == 1 || limelight.getApriltag() == 8) {
-      autoChooser.addOption("Two cube auto", 7);
-      autoChooser.addOption("Leave Community", 3);
-      autoChooser.addOption("One Cube Auto", 5);
-    }
-
-    else {
-      autoChooser.addOption("Two cube auto", 6);
-      autoChooser.addOption("Leave Community", 3);
-      autoChooser.addOption("One Cube Auto", 5);
-    }
-
-    autoModeSelection = 0;
-    
-    leds = new LEDs();
-    hue = 0;   
-    value = 255;
-    FMS_Connected = false;
-    gamePiece = "Cube";
-    robotState = "stow";
-    currentState = "stow";
-
-    DriverStation.silenceJoystickConnectionWarning(true);
-    CameraServer.startAutomaticCapture();
-    compressor.enableDigital();
+    m_chooser.setDefaultOption("Default Auto", kDefaultAuto);
+    m_chooser.addOption("My Auto", kCustomAuto);
+    SmartDashboard.putData("Auto choices", m_chooser);
   }
 
   /**
@@ -161,32 +39,7 @@ public class Robot extends TimedRobot {
    * SmartDashboard integrated updating.
    */
   @Override
-  public void robotPeriodic() {
-    autoModeSelection = (int) SmartDashboard.getNumber("Auton Mode", 0.0);
-    SmartDashboard.putData("Auto Selector", autoChooser);
-    SmartDashboard.putNumber("Auton Mode", autoModeSelection);
-    SmartDashboard.putString("game piece", gamePiece);
-
-    if(gamePiece == "Cone") {
-      grabber.closeGrabber();
-      hue = 15;
-    }
-    else if(gamePiece == "Cube") {
-      grabber.openGrabber();
-      hue = 130;
-    }
-
-    swerve.logging();
-    limelight.logging();
-    elevator.logger();
-    SmartDashboard.putNumber("Elevator Target", elevatorTarget);
-    grabber.logging();
-    SmartDashboard.putNumber("Arm Target", armTarget);
-    intake.logging();
-    SmartDashboard.putString("Robot State", robotState);
-
-    leds.sendData();
-  }
+  public void robotPeriodic() {}
 
   /**
    * This autonomous (along with the chooser code above) shows how to select between different
@@ -200,249 +53,32 @@ public class Robot extends TimedRobot {
    */
   @Override
   public void autonomousInit() {
-    autoModeRunner.chooseAutoMode(autoModes[autoModeSelection]);
-    autoModeRunner.start();   
+    m_autoSelected = m_chooser.getSelected();
+    // m_autoSelected = SmartDashboard.getString("Auto Selector", kDefaultAuto);
+    System.out.println("Auto selected: " + m_autoSelected);
   }
 
   /** This function is called periodically during autonomous. */
   @Override
   public void autonomousPeriodic() {
-
+    switch (m_autoSelected) {
+      case kCustomAuto:
+        // Put custom auto code here
+        break;
+      case kDefaultAuto:
+      default:
+        // Put default auto code here
+        break;
+    }
   }
+
   /** This function is called once when teleop is enabled. */
   @Override
-  public void teleopInit() {
-    autoModeRunner.stop();
-
-    swerve.defaultNeutralMode(true, true);
-    leds.setRGB(0, 0, 0);
-  }
+  public void teleopInit() {}
 
   /** This function is called periodically during operator control. */
   @Override
-  public void teleopPeriodic() {
-    /* Driver Controls */
-
-    /* vision align */
-    if(driver.A.isPressed()) {
-      double[] speeds = limelight.fullAlign();
-      swerve.teleopDrive(limelight.distanceVisionAlign(), limelight.XVisionAlign(), 0, true);
-    }
-    /* rezero robot heading */
-    else if(driver.START.isPressed()) {
-      swerve.zeroHeading();
-    }
-    /* creep drive */
-    else if(driver.LT.isPressed()) {
-      swerve.teleopDrive(Constants.Swerve.creepSpeed*driver.LeftY.getTrueAxis(), Constants.Swerve.creepSpeed*driver.LeftX.getTrueAxis(), Constants.Swerve.creepSpeed*driver.RightX.getTrueAxis(), false);
-    }
-    /* regular drive */
-    else {
-      swerve.teleopDrive(driver.LeftY.getTrueAxis(), driver.LeftX.getTrueAxis(), driver.RightX.getTrueAxis(), false);
-    }
-
-    /* robot States */
-    if(robotState == "stow" && navigator.GREEN.isPressed()) {
-      grabber.zeroArm();
-    }
-    else if(navigator.RED.isPressed()) {
-      grabber.ArmRot(0);
-    }
-    else if(navigator.DPAD.getPOV() == 0) {
-      grabber.ArmRot(0.2);
-    }
-    else if(navigator.DPAD.getPOV() == 180) {
-      grabber.ArmRot(-0.2);
-    } 
-    else {
-      grabber.magicArm(armTarget);   
-    }
-    elevator.magicElevator(elevatorTarget);
-
-    // reset arm zero position
-    if(navigator.GREEN.isPressed() && robotState == "stow") {
-      grabber.zeroArm();
-    }
-
-    if(driver.dPad.getPOV() == 0) { /* high score state */
-      /*elevatorTarget = Constants.ElevatorConstants.ELEVATOR_FEEDER_EXTEND;
-      if(elevator.getPosition() > Constants.ElevatorConstants.ELEVATOR_WAIT) {
-        armTarget = Constants.GrabberConstants.ARM_HIGH_EXTEND;
-      }*/
-      robotState = "high";
-    }
-    else if(driver.dPad.getPOV() == 90) { /* mid score state */
-      /*elevatorTarget = Constants.ElevatorConstants.ELEVATOR_MID_EXTEND;
-      if(elevator.getPosition() > Constants.ElevatorConstants.ELEVATOR_WAIT) {
-        armTarget = Constants.GrabberConstants.ARM_MID_EXTEND;
-      }*/  
-      robotState = "mid";
-    }
-    else if(driver.dPad.getPOV() == 270) { /* low score state */
-      /*elevatorTarget = Constants.ElevatorConstants.ELEVATOR_LOW_EXTEND;
-      if(elevator.getPosition() > Constants.ElevatorConstants.ELEVATOR_WAIT) {
-        armTarget = Constants.GrabberConstants.ARM_LOW_EXTEND;
-      }*/
-      robotState = "low";
-    }
-    else if(driver.dPad.getPOV() == 180) { /* stow state */
-      /*elevatorTarget = Constants.ElevatorConstants.ELEVATOR_BOTTTOM_EXTEND;
-      armTarget = Constants.GrabberConstants.ARM_STOWED_EXTEND;*/
-      robotState = "stow";
-    }
-    else if(driver.Y.isPressed()) { /* feeder state */
-      /*elevatorTarget = Constants.ElevatorConstants.ELEVATOR_FEEDER_EXTEND;
-      if(elevator.getPosition() > Constants.ElevatorConstants.ELEVATOR_WAIT) {
-        armTarget = Constants.GrabberConstants.ARM_FEEDER_EXTEND;
-      }*/
-      robotState = "feeder";
-    }
-
-
-    if(driver.RB.isPressed()) {
-      if(attemptingConeGrab) {
-        robotState = "attemptCone";
-      }
-      else if(intakeCone && !attemptingConeGrab) {
-        robotState = "intakeCone";
-      }
-    }
-    
-    else if(driver.LB.isPressed()) {
-      //grabber.runGrabber(-Constants.GrabberConstants.GRABBER_SPEED);
-      intakeCone = false;
-      grabber.runGrabberNoSensor(-Constants.GrabberConstants.GRABBER_SPEED);
-    }
-
-    else if (!passthrough) {
-      grabber.runGrabberPassthrough(0);
-    }
-
-    if (attemptingConeGrab && !driver.RB.isPressed()) {
-      armTarget = Constants.GrabberConstants.ARM_FEEDER_EXTEND;
-      attemptingConeGrab = false;
-    }
-
-    // intake controls
-    if(driver.B.isPressed()) {
-      intake.ejectGamePiece();
-    }
-    else if(driver.RT.isPressed()) {
-      intake.intakeGamePiece();
-    }
-    else {
-      intake.idleGamePiece();
-    }
-
-    
-    if(driver.BACK.isPressed()) {
-      passthrough = false;
-    }
-
-    if(driver.X.isPressed()) {
-      if (grabber.getLimitSwitch()){
-        passthrough = true;
-      }
-      
-    }
-
-    
-
-    if(passthrough) {
-      intake.passthrough(grabber.getLimitSwitch());
-      grabber.runGrabberPassthrough(Constants.GrabberConstants.GRABBER_SPEED);
-      gamePiece = "Cube";
-      if (!grabber.getLimitSwitch()){
-        passthrough = false;
-      }
-    }
-
-
-
-    // leds and game piece determinant
-    value = 128 + (int)(navigator.WAMMY.getTrueAxis() * 127);
-    leds.setHSV(hue, 255, value);
-    if(navigator.BLUE.isPressed()) {
-      hue = 130;
-      gamePiece = "Cube";
-      }
-    else if(navigator.YELLOW.isPressed()) {
-      hue = 15;
-      gamePiece = "Cone";
-      }
-      else if(navigator.ORANGE.isPressed()) {
-        hue = 7;
-      }
-
-      switch(robotState){
-        case "stow":
-          elevatorTarget = Constants.ElevatorConstants.ELEVATOR_BOTTTOM_EXTEND;
-          armTarget = Constants.GrabberConstants.ARM_STOWED_EXTEND;
-          attemptingConeGrab = false;
-          intakeCone = false;
-          break;
-        case "low":
-          elevatorTarget = Constants.ElevatorConstants.ELEVATOR_LOW_EXTEND;
-          if(elevator.getPosition() > Constants.ElevatorConstants.ELEVATOR_WAIT) {
-            armTarget = Constants.GrabberConstants.ARM_LOW_EXTEND;
-          }
-          attemptingConeGrab = false;
-          intakeCone = true;
-          currentState = "low";
-          break;
-        case "mid":
-          elevatorTarget = Constants.ElevatorConstants.ELEVATOR_MID_EXTEND;
-          if(elevator.getPosition() > Constants.ElevatorConstants.ELEVATOR_WAIT) {
-            armTarget = Constants.GrabberConstants.ARM_MID_EXTEND;
-          }
-          attemptingConeGrab = false;
-          intakeCone = true;
-          currentState = "mid";
-          break;
-        case "high":
-          elevatorTarget = Constants.ElevatorConstants.ELEVATOR_FEEDER_EXTEND;
-          if(elevator.getPosition() > Constants.ElevatorConstants.ELEVATOR_WAIT) {
-            armTarget = Constants.GrabberConstants.ARM_HIGH_EXTEND;
-          }
-          attemptingConeGrab = false;
-          intakeCone = true;
-          currentState = "high";
-          break;
-        case "feeder":
-          elevatorTarget = Constants.ElevatorConstants.ELEVATOR_FEEDER_EXTEND;
-          if(elevator.getPosition() > Constants.ElevatorConstants.ELEVATOR_WAIT) {
-            armTarget = Constants.GrabberConstants.ARM_FEEDER_EXTEND;
-          }
-          attemptingConeGrab = true;
-          intakeCone = false;
-          break;
-        case "attemptCone":
-          grabber.runGrabber(Constants.GrabberConstants.GRABBER_SPEED);
-          if(!driver.RB.isPressed()) {
-            armTarget = Constants.GrabberConstants.ARM_FEEDER_EXTEND;
-            robotState = "feeder";
-            break;
-          }
-          if(!grabber.getBeamBreakOutside()) {
-            armTarget = Constants.GrabberConstants.ARM_FEEDER_EXTEND;
-          }
-          else if(!grabber.getBeamBreakInside()) {
-            armTarget = Constants.GrabberConstants.ARM_FEEDER_DROPPED_EXTEND;
-          }
-          break;
-        case "intakeCone":
-          if(!driver.RB.isPressed()) {
-            grabber.runGrabber(0);
-            robotState = currentState;
-          }
-          else{
-            grabber.runGrabberNoSensor(Constants.GrabberConstants.GRABBER_SLOW_SPEED);
-          }
-          break;
-        default:
-          robotState = "stow";
-      }
-    }
+  public void teleopPeriodic() {}
 
   /** This function is called once when the robot is disabled. */
   @Override
@@ -450,48 +86,20 @@ public class Robot extends TimedRobot {
 
   /** This function is called periodically when disabled. */
   @Override
-  public void disabledPeriodic() {
-    value = 128 + (int)(navigator.WAMMY.getTrueAxis() * 127);
-
-    if(FMS_Connected) {
-      if(navigator.GREEN.isPressed()) {
-        hue = 60;
-      }
-      else if(navigator.RED.isPressed()) {
-        hue = 0;
-      }
-      else if(navigator.YELLOW.isPressed()) {
-        hue = 15;
-      }
-      else if(navigator.BLUE.isPressed()) {
-        hue = 130;
-      }
-      else if(navigator.ORANGE.isPressed()) {
-        hue = 7;
-      }
-      leds.setHSV(hue, 255, value);
-    }
-    else {
-      if(DriverStation.getAlliance() == Alliance.Red && DriverStation.isDSAttached()) {
-        hue = 0;
-        FMS_Connected = true;
-      }
-      else if(DriverStation.getAlliance() == Alliance.Blue && DriverStation.isDSAttached()) {
-        hue = 130;
-        FMS_Connected = true;
-      }
-    }
-  }
+  public void disabledPeriodic() {}
 
   /** This function is called once when test mode is enabled. */
   @Override
   public void testInit() {}
+
   /** This function is called periodically during test mode. */
   @Override
   public void testPeriodic() {}
+
   /** This function is called once when the robot is first started up. */
   @Override
   public void simulationInit() {}
+
   /** This function is called periodically whilst in simulation. */
   @Override
   public void simulationPeriodic() {}
