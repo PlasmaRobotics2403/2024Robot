e042a4370d862e5fbd40ed7a38cd11dbbac8b810
ethan <ethan@diavida.com>
2024-02-02 19:25:46 -0700

    adding swerve to master

---
+++ src/main/java/frc/robot/subsystems/Swerve.java
@@ -0,0 +1,85 @@
+package frc.robot.subsystems;
+
+import com.ctre.phoenix6.mechanisms.swerve.SwerveDrivetrain;
+import com.ctre.phoenix6.mechanisms.swerve.SwerveDrivetrainConstants;
+import com.ctre.phoenix6.mechanisms.swerve.SwerveModule.DriveRequestType;
+import com.ctre.phoenix6.mechanisms.swerve.SwerveRequest;
+
+import edu.wpi.first.math.geometry.Rotation2d;
+import frc.robot.Constants.SwerveConstants;
+import frc.java.generated.TunerConstants;
+
+public class Swerve {
+    private SwerveDrivetrain drivetrain;
+
+    // request types
+    private final SwerveRequest.SwerveDriveBrake brake = new SwerveRequest.SwerveDriveBrake();
+    private final SwerveRequest.PointWheelsAt point = new SwerveRequest.PointWheelsAt();
+    private final SwerveRequest.FieldCentric drive = new SwerveRequest.FieldCentric()
+        .withDriveRequestType(DriveRequestType.OpenLoopVoltage);    // I want field-centric
+                                                                    // driving in open loop
+
+    /**
+     * constructor for swerve utilizing values from 
+     * tuner X swerve generator.
+     */
+    public Swerve() {
+        drivetrain = new SwerveDrivetrain(
+            SwerveConstants.DrivetrainConstants,
+            SwerveConstants.FrontLeft,
+            SwerveConstants.FrontRight,
+            SwerveConstants.BackLeft,
+            SwerveConstants.BackRight
+        );
+    }
+
+    /**
+     * set swerve modules to X pattern
+     */
+    public void swerveBrake() {
+        drivetrain.setControl(brake);
+    }
+
+    /**
+     * point wheels in given direction
+     * @param xComponent x direction to point
+     * @param yComponent y direction to point
+     */
+    public void swervePoint(double xComponent, double yComponent) {
+        drivetrain.setControl(point.withModuleDirection(new Rotation2d(-xComponent, -yComponent)));
+    }
+
+    /**
+     * field centric swerve drive
+     * @param xSpeed speed the robot drives forward
+     * @param ySpeed speed the robot strafes
+     * @param rotation speed the robot rotates
+     */
+    public void swerveFieldCentric(double xSpeed, double ySpeed, double rotation) {
+        drivetrain.setControl(drive.withVelocityX(-xSpeed * SwerveConstants.maxSpeed)
+                                   .withVelocityY(-ySpeed * SwerveConstants.maxSpeed)
+                                   .withRotationalRate(-rotation * SwerveConstants.maxAngularRate)
+        );
+    }
+
+    /**
+     * re-orient field centric to current heading
+     */
+    public void resetHeading() {
+        drivetrain.seedFieldRelative();
+    }
+
+    /**
+     * SmartDashboard logging info
+     */
+    private void logging() {
+    }
+
+    /**
+     * goes in robot periodic to trigger any
+     * important updates.
+     */
+    public void periodic() {
+        logging();
+    }
+}
